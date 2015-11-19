5bfc0d58aa51c6171aaec81209cc4287308ad664
Joel <j.n.brewster@gmail.com>
2015-11-19 21:45:25 +0900

    Add Falsybouncer.js

---
+++ Falsybouncer.js
@@ -0,0 +1,20 @@
+//Bonfire: Falsy Bouncer
+/*
+Remove all falsy values from an array.
+
+Falsy values in javascript are false, null, 0, "", undefined, and NaN.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+Here are some helpful links:
+
+Boolean Objects
+Array.filter()
+*/
+function bouncer(arr) {
+  // Don't show a false ID to this bouncer.
+  return arr.filter(Boolean);
+}
+
+bouncer([7, "ate", "", false, 9]);
+
